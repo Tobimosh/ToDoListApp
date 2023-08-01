13785a04dd9b2c6661626c0b88be3ea3019f2c3c
Oluwatobi Moshood <oluwatobimoshood16@gmail.com>
2023-08-01 18:39:21 +0100

    implemented the windows local storage functionality

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ window.addEventListener('load', () => {
     const inputText = document.querySelector('#input-task');
     const taskList = document.querySelector('#tasks');
 
-    form.addEventListener('submit', (e) =>{
+    // Load existing tasks from local storage on page load
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    for (const savedTask of savedTasks) {
+        addTaskToTaskList(savedTask);
+    }
+
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
         const task = inputText.value;
-        if(!task){
+        if (!task) {
             alert("Please fill out the task.")
             return;
         }
 
+        addTaskToTaskList(task);
+
+        // Save tasks to local storage
+        saveTasksToLocalStorage();
+
+        inputText.value = "";
+    });
+
+    function addTaskToTaskList(task) {
         const task_el = document.createElement("div");
         task_el.classList.add("task");
 
         const task_content_el =  document.createElement("div");
         task_content_el.classList.add("content");
-        //task_content_el.innerText = task;
-
-        task_el.appendChild(task_content_el);
 
         const task_input = document.createElement('input');
         task_input.type = "text"
@@ -27,7 +39,6 @@ window.addEventListener('load', () => {
         task_input.setAttribute("readonly", "readonly");
 
         task_content_el.appendChild(task_input);
-
 
         const task_actions = document.createElement("div");
         task_actions.classList.add("actions");
@@ -43,25 +54,38 @@ window.addEventListener('load', () => {
         task_actions.appendChild(task_edit);
         task_actions.appendChild(task_delete);
 
+        task_el.appendChild(task_content_el);
         task_el.appendChild(task_actions);
         taskList.appendChild(task_el);
 
-        inputText.value = "";
-        
-        task_edit.addEventListener('click', () =>{
-            if(task_edit.innerText.toLowerCase() == "edit"){
+        task_edit.addEventListener('click', () => {
+            if (task_edit.innerText.toLowerCase() == "edit") {
                 task_input.removeAttribute("readonly");
                 task_input.focus();
                 task_edit.innerText = "Save";
-
-            }
-            else{
+            } else {
                 task_input.setAttribute("readonly", "readonly");
                 task_edit.innerText = "Edit"; 
             }
+
+            // Save tasks to local storage after editing
+            saveTasksToLocalStorage();
         });
-    })
 
-    
+        task_delete.addEventListener('click', () => {
+            taskList.removeChild(task_el);
 
-}) 
+            // Save tasks to local storage after deletion
+            saveTasksToLocalStorage();
+        });
+    }
+
+    function saveTasksToLocalStorage() {
+        const tasks = [];
+        const taskElements = taskList.querySelectorAll('.text');
+        for (const taskElement of taskElements) {
+            tasks.push(taskElement.value);
+        }
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+});
